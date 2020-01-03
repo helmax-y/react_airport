f4d6a01c4617b3395bcda984c52705ec50cbae7c
helmax-y <max_rad@ukr.net>
2020-01-03 22:02:28 +0200

    Updates

---
+++ precache-manifest.b896c88eae534b505f0d10e679bae04a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc8c32dbb8da50925fc3313a67ee7ddd",
+    "url": "/react_airport/index.html"
+  },
+  {
+    "revision": "96015e2902a49396d539",
+    "url": "/react_airport/static/css/main.4db258b9.chunk.css"
+  },
+  {
+    "revision": "db8455ba4cce77af2bff",
+    "url": "/react_airport/static/js/2.54a21c52.chunk.js"
+  },
+  {
+    "revision": "96015e2902a49396d539",
+    "url": "/react_airport/static/js/main.b1fbbd14.chunk.js"
+  },
+  {
+    "revision": "c8cd8e70b1c2d1cd9765",
+    "url": "/react_airport/static/js/runtime~main.037fa8df.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_airport/precache-manifest.2b1cc2bfbee41366a0270597566a8204.js"
+  "/react_airport/precache-manifest.b896c88eae534b505f0d10e679bae04a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_airport/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
